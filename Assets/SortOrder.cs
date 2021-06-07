7a5dcc7eb3d6fcf33388d8883f37f4e99627b133
Amir Aharoni <65896568+TheWize@users.noreply.github.com>
2021-06-08 02:36:33 +0300

    sortMode on

--- Assets/SortOrder.cs
+++ Assets/SortOrder.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class SortOrder : MonoBehaviour
 {
     [SerializeField] private bool doUpdate;
-    
+    private SpriteRenderer m_sr;
+    private float height = 7;
     void Start()
     {
-        
+        m_sr = GetComponent<SpriteRenderer>();
+        m_sr.sortingOrder = getSort();
+        StartCoroutine(doUpdateSortCoru());
     }
-
-    void LateUpdate()
+    private IEnumerator doUpdateSortCoru()
     {
-        
+        while (doUpdate)
+        {
+            m_sr.sortingOrder = getSort();
+            yield return new WaitForEndOfFrame();
+        }
     }
+    private int getSort() {
+        return Mathf.RoundToInt(-transform.position.y * 10);
+    } 
 }
