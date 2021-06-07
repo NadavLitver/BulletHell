b5d4ed045ddb34b5a3aefceb6be05fc5803803e4
Amir Aharoni <65896568+TheWize@users.noreply.github.com>
2021-06-07 23:37:47 +0300

    Sorting layers 0.5

---
+++ Assets/SortOrder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortOrder : MonoBehaviour
+{
+    [SerializeField] private bool doUpdate;
+    
+    void Start()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        
+    }
+}
