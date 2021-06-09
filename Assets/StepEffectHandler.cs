368e2b7144eafbbb4a520e22b14f94191f30ae36
Amir Aharoni <65896568+TheWize@users.noreply.github.com>
2021-06-09 16:30:33 +0300

    smol update
    no implementations made

---
+++ Assets/StepEffectHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepEffectHandler : MonoBehaviour
+{
+    public GameObject step;
+    public void PlayWithOffset(Vector3 offset)
+    {
+        Instantiate(step, transform.position + offset, Quaternion.identity);
+    }
+}
