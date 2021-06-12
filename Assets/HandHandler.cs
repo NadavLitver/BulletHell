0ff5fe612d5a9ca9b3699f14681aca56ee17b3f6
Nadav Litver <Nlitver@gmail.com>
2021-06-12 19:49:28 +0300

    Ankh

---
+++ Assets/HandHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandHandler : MonoBehaviour
+{
+    [SerializeField]
+    Transform FirePoint;
+
+    private void Update()
+    {
+        transform.rotation = new Quaternion(FirePoint.rotation.x, FirePoint.rotation.y, -FirePoint.rotation.z + 90f,FirePoint.rotation.w);
+    }
+
+}
