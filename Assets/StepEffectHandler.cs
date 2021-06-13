44a66789cb6efc1ae03089bdebca8eadf289f74f
Amir Aharoni <65896568+TheWize@users.noreply.github.com>
2021-06-13 23:56:07 +0300

    gentle push in the right direction

--- Assets/StepEffectHandler.cs
+++ Assets/StepEffectHandler.cs
@@ -5,8 +5,23 @@ using UnityEngine;
 public class StepEffectHandler : MonoBehaviour
 {
     public GameObject step;
-    public void PlayWithOffset(Vector3 offset)
+
+    [SerializeField] private Transform pivotMid;
+    [SerializeField] private Transform pivotLeft;
+    [SerializeField] private Transform pivotRight;
+
+
+    private Vector3 rotation = new Vector3(60f,0f,0f);
+    public void PlayMid()
     {
-        Instantiate(step, transform.position + offset, Quaternion.identity);
+        Instantiate(step, pivotMid.position, Quaternion.Euler(rotation));
+    }
+    public void PlayLeft()
+    {
+        Instantiate(step, pivotLeft.position, Quaternion.Euler(rotation));
+    }
+    public void PlayRight()
+    {
+        Instantiate(step, pivotRight.position, Quaternion.Euler(rotation));
     }
 }
---
+++ Assets/Torch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torch : MonoBehaviour
+{
+    Animator m_anim;
+
+    [SerializeField] private float minSpeed;
+    [SerializeField] private float maxSpeed;
+    private void Awake()
+    {
+        m_anim = GetComponent<Animator>();
+        m_anim.speed = Random.Range(minSpeed, maxSpeed);
+    }
+
+
+}
