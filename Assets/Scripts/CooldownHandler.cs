c19e93be8aae8d23242c71a396bcf1fa5432e962
Bar Ronen <barzie02@gmail.com>
2021-08-05 16:54:41 +0300

    OrbHandler + CooldownHandler

---
+++ Assets/Scripts/CooldownHandler.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CooldownHandler : MonoBehaviour
+{
+    public Image cooldownImage;
+
+    private bool inCooldown = false;
+    private float cooldownTime;
+
+    public KeyCode abilityKey;
+
+    public PlayerShooter PlayerShooterInstance;
+
+    [HideInInspector] public enum Abilities {AbilityOne, AbilityTwo, TeleportAbility };
+    public Abilities abilitiesRef;
+
+
+    private void Start()
+    {
+        cooldownImage.fillAmount = 0f;
+    }
+
+    private void Update()
+    {
+        switch (abilitiesRef)
+        {
+            case Abilities.AbilityOne:
+                Holyshock();
+                break;
+            case Abilities.AbilityTwo:
+                SpecialAbility();
+                break;
+            case Abilities.TeleportAbility:
+                DoorOfLight();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void Holyshock()
+    {
+        if (inCooldown)
+        {
+            cooldownImage.fillAmount -= 1 / cooldownTime * Time.deltaTime;
+            if (cooldownImage.fillAmount <= 0)
+            {
+                cooldownImage.fillAmount = 0;
+                inCooldown = false;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            cooldownTime = PlayerShooterInstance.holyShockCD;
+            if (!inCooldown)
+            {
+                inCooldown = true;
+                cooldownImage.fillAmount = 1;
+            }
+
+        }
+    }
+
+    private void SpecialAbility()
+    {
+        if (inCooldown)
+        {
+            cooldownImage.fillAmount -= 1 / cooldownTime * Time.deltaTime;
+            if (cooldownImage.fillAmount <= 0)
+            {
+                cooldownImage.fillAmount = 0;
+                inCooldown = false;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            cooldownTime = Inventory.InventoryInstace.specialAbility.runningCD;
+
+            if (!inCooldown)
+            {
+                inCooldown = true;
+                cooldownImage.fillAmount = 1;
+            }
+        }
+    }
+
+    private void DoorOfLight()
+    {
+        if (inCooldown)
+        {
+            cooldownImage.fillAmount -= 1 / cooldownTime * Time.deltaTime;
+            if (cooldownImage.fillAmount <= 0)
+            {
+                cooldownImage.fillAmount = 0;
+                inCooldown = false;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            cooldownTime = PlayerShooterInstance.teleportCD;
+
+            if (!inCooldown)
+            {
+                inCooldown = true;
+                cooldownImage.fillAmount = 1;
+            }
+
+
+        }
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/OrbHandler.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class OrbHandler : MonoBehaviour
+{
+    public Material m_mat;
+
+    // Liquid shader properties
+    private const string LiquidStrenghtREF = "_LiquidStr";
+    private const string BounceSpeedREF = "_BouceSpeed";
+
+
+    [Range(-0.7f, 0.7f)] private float curVal;
+    private float tempRange = 0.7f;
+
+
+    private void Start()
+    {
+        curVal = 0.7f;
+        m_mat.SetFloat(LiquidStrenghtREF, curVal);
+
+    }
+
+    private void Update()
+    {
+        SetLiquidBounce();
+    }
+
+    public void SetHP(float playerHP)
+    {
+        curVal = Mathf.Lerp(-0.7f, 0.7f, playerHP / 100);
+        m_mat.SetFloat(LiquidStrenghtREF, curVal);
+    }
+
+    private void SetLiquidBounce()
+    {
+        
+        m_mat.SetFloat(BounceSpeedREF, 1.5f);
+
+        
+    }
+
+
+
+
+
+}
