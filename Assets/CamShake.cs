d221e689270efe44e14e09910ef8bb1e5f003007
Amir Aharoni <65896568+TheWize@users.noreply.github.com>
2021-08-05 16:18:26 +0300

    cam shake

---
+++ Assets/CamShake.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CamShake : MonoBehaviour
+{
+    public static CamShake instance;
+    private CinemachineVirtualCamera m_cam;
+    private float shakeTimer;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void Start()
+    {
+        m_cam = GetComponent<CinemachineVirtualCamera>();
+    }
+    public void callCamShake(float duration, float intensity)
+    {
+        StartCoroutine(camShakeCoru(duration, intensity));
+    }
+    private IEnumerator camShakeCoru(float duration, float intensity)
+    {
+        float currDurr = 0;
+        Debug.Log("camshake start");
+        while (currDurr < 1)
+        {
+            CinemachineBasicMultiChannelPerlin perlin = m_cam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+            currDurr += Time.deltaTime / duration;
+            perlin.m_AmplitudeGain = Mathf.Lerp(intensity, 0, currDurr);
+            yield return new WaitForEndOfFrame();
+        }
+        Debug.Log("camshake stop");
+    }
+}
---
+++ Assets/PlayerHitEffect.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHitEffect : MonoBehaviour
+{
+    private SpriteRenderer m_sprite;
+    private void Start()
+    {
+        m_sprite = GetComponent<SpriteRenderer>();
+    }
+    public void CallHitEffect()
+    {
+        StartCoroutine(HitEffect());
+    }
+    private IEnumerator HitEffect()
+    {
+        float currDurr = 0;
+        float duration = .3f;
+        CamShake.instance.callCamShake(duration, 1);
+        while (currDurr < 1)
+        {
+            m_sprite.color = Color.Lerp(Color.black, Color.white, currDurr);
+            currDurr += Time.deltaTime / duration;
+            yield return new WaitForEndOfFrame();
+        }
+        m_sprite.color = Color.white;
+
+    }
+}
