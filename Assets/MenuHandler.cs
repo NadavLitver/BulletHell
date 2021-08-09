92e34e12b6759437302f254a2d7212f1f0f039b0
Bar Ronen <barzie02@gmail.com>
2021-08-09 18:47:28 +0300

    Main+Credits menu

--- Assets/MenuHandler.cs
+++ Assets/MenuHandler.cs
@@ -1,26 +1,42 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuHandler : MonoBehaviour
 {
+    public GameObject MainMenuCanvas;
+    public GameObject MainMenu;
+    public GameObject CreditsCanvas;
+    public GameObject CreditsMenu;
+
+
+
 
     public void PlayButton()
     {
-        StartCoroutine(PlayButtonSequence());
+        SceneManager.LoadScene(1);
+    }
+
+    public void CreditsButton()
+    {
+        MainMenu.SetActive(false);
+        MainMenuCanvas.SetActive(false);
+        CreditsMenu.SetActive(true);
+        CreditsCanvas.SetActive(true);
+    }
+
+    public void BackButton()
+    {
+        CreditsMenu.SetActive(false);
+        CreditsCanvas.SetActive(false);
+        MainMenu.SetActive(true);
+        MainMenuCanvas.SetActive(true);
+
     }
 
     public void QuitGame()
     {
         Application.Quit();
     }
-
-    public IEnumerator PlayButtonSequence()
-    {
-        yield return new WaitForSeconds(0.5f);
-        SceneManager.LoadScene(1);
-    }
-
 
 }
